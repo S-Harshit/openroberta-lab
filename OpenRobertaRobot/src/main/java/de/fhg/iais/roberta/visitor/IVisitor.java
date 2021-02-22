328d2d9dbb9f775ba3d81617ad48b28af2dfe794
Ayk Borstelmann <ayk.borstelmann@iais.fraunhofer.de>
2021-02-22 12:26:48 +0100

    test IVisitor#visit and improve method

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/IVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/IVisitor.java
@@ -1,5 +1,6 @@
 package de.fhg.iais.roberta.visitor;
 
+import de.fhg.iais.roberta.syntax.Phrase;
 import de.fhg.iais.roberta.util.dbc.DbcException;
 
 import java.lang.reflect.InvocationTargetException;
@@ -9,7 +10,12 @@ import java.lang.reflect.Method;
  * Interface to be used with the visitor pattern to traverse an AST (and generate code, e.g.).
  */
 public interface IVisitor<V> {
-    default V visit(Object visitable) {
+    /**
+     * Delegates visitable to matching visitT(T t) method from substructure
+     * @param visitable meant to be visited
+     * @return output of delegated visit-method
+     */
+    default V visit(Phrase<V> visitable) {
         String className = visitable.getClass().getSimpleName();
         String methodName = String.format("visit%s", className);
         try {
@@ -18,7 +24,7 @@ public interface IVisitor<V> {
             V result = (V) m.invoke(this, new Object[]{visitable});
             return result;
         } catch ( NoSuchMethodException e ) {
-            throw new DbcException(String.format("visit Method \"%s\" not found on \"%s\" (Block \"%s\" not supported)", methodName, this.getClass().getSimpleName(), className), e);
+            throw new DbcException(String.format("visit Method \"%s\" not found on \"%s\" (Block \"%s\" is probably not supported)", methodName, this.getClass().getSimpleName(), className), e);
         } catch ( IllegalAccessException e ) {
             throw new DbcException(e);
         } catch ( InvocationTargetException e ) {
